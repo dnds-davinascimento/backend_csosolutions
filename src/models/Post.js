072ee96d16158c0df9f96b7bcf9335187e8b38ba
DAVI <davinasc98@gmail.com>
2023-02-18 11:23:18 -0300

    test7

--- src/models/Post.js
+++ src/models/Post.js
@@ -22,7 +22,7 @@ const postSchema = new Schema( {
 { toJSON:{ virtuals: true,},
 });
     postSchema.virtual('foto_url').get(function(){
-    return `http://localhost:4000/files/${this.foto}`
+    return `http://backend-csosolutions.vercel.app/files/${this.foto}`
     //backend-csosolutions.vercel.app
 })
 
